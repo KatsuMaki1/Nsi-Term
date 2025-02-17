b80ea61345c98859a4beb1b4d150091311421bd6
SkyOxyFR <truffierblancthomas@gmail.com>
2025-02-17 08:27:11 +0100

    Update rendu_monnaie.py
    NICE

--- Sky_Oxy/rendu_monnaie.py
+++ Sky_Oxy/rendu_monnaie.py
@@ -1,27 +1,23 @@
 INF = 999999999999
 
 def rendu_monnaie_dyn(P, X, memo):
+    mini = INF
     if X == 0:
         return 0
-    if X in memo:
-        return memo[X]
-    
-    mini = INF
-    for piece in P:
-        if piece <= X:
-            nb = 1 + rendu_monnaie_dyn(P, X - piece, memo)
-            mini = min(mini, nb)
-    
-    memo[X] = mini
+    elif memo[X]> 0:
+        return 
+    else : 
+        for i in range (len(P)):
+            if P[i] <= X:
+                nb = 1 + rendu_monnaie_dyn(P, X - P[i],)
+                if nb<mini:
+                    mini = nb
+                    memo[X] = mini
     return mini
 
-# Liste des pièces disponibles
 pieces = [2, 5, 10, 50, 100]
-# Montant à rendre
 somme = 171
-# Dictionnaire pour la mémoïsation
 memo = {}
 
-# Calcul du nombre minimal de pièces
 resultat = rendu_monnaie_dyn(pieces, somme, memo)
 print(f"Nombre minimal de pièces pour rendre {somme} cts : {resultat}")
